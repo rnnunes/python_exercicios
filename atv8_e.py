f0c6d6a4ba585b683469663689256dcec619d547
Rian Nunes <ryannunes70@gmail.com>
2023-01-11 22:58:19 -0400

    commiti

--- atv8_e.py
+++ atv8_e.py
@@ -1,5 +1,25 @@
 
 
-print('=== BANCO - CAIXA ELETRONICO ===')
+print('=== BANCO - CAIXA ELETRONICO - ERRO VERIFICAR - TEM Q MELHORAR ===')
 
 saque = int(input('Que valor voçê quer sacar? R$: '))
+total = saque
+cedula = 50
+cont_cedula = 0
+
+
+while True:
+    if total >= cedula:
+        total -= cedula
+        cont_cedula += 1
+    else:
+        if cont_cedula > 0:
+            print('Total de Cedulas {}, Cedulas de R$ R$ {}.'.format(cont_cedula,cedula))
+            if cedula == 50:
+                    cedula = 20
+            elif cedula == 20:
+                cedula = 10
+            elif cedula == 10:
+                cedula = 1
+            if total == 0:
+                    break
---
+++ atv9.py
@@ -0,0 +1,10 @@
+
+
+
+print('=== TUPLAS ===')
+
+contagem_ext = ('Zero', 'Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez',
+'Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+
+for cont, ext in enumerate(contagem_ext):
+    print('N° {} - {}'.format(cont, ext))
