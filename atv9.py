1bd357e8f8fdbb9fbe4c8869244395517cc35e41
Rian Nunes <ryannunes70@gmail.com>
2023-01-12 23:07:39 -0400

    commit

--- atv9.py
+++ atv9.py
@@ -1,10 +1,13 @@
 
-
-
 print('=== TUPLAS ===')
 
 contagem_ext = ('Zero', 'Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez',
 'Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+cont_num = len(contagem_ext)
 
-for cont, ext in enumerate(contagem_ext):
-    print('N° {} - {}'.format(cont, ext))
+while True:
+    questao = int(input('Digite um número entre 0 e 20: '))
+    if  0 <=  questao <= 20:
+        break
+
+print('Voçê digitou o numero {}'.format(contagem_ext[questao]))
