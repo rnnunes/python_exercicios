f54c3a4b42ab4aa37780735b07eb0363965e83ac
Rian Nunes <ryannunes70@gmail.com>
2023-01-16 00:09:02 -0400

    alt

--- atv10_a.py
+++ atv10_a.py
@@ -4,17 +4,26 @@
 
 
 lista = []
+maior = 0
+menor = 0
 
 for c in range(0,5):
     lista.append(input('Insira um novo valor {}°:'.format(c)))
+    if c == 0:
+        maior = menor = lista[c]
+    else:
+        if lista[c] > maior:
+            maior = lista[c]
+        if lista[c] < menor:
+            menor = lista[c]
 
-for pos, cont in enumerate (lista):
-    print('Posição {} Valor {}'.format(pos, cont))
-
-print('Maior valor foi {} na posição {}.'.format(max(lista), pos), end='')
+print('Maior valor foi {} na posição '.format(maior), end='')
 for i, v in enumerate(lista):
-    if v == max(lista):
+    if v == maior:
         print('{}...'.format(i), end='')
 print('')
 
-print('Menor valor foi {} na posição {}.'.format(min(lista)), end='')
+print('Menor valor foi {} na posição '.format(menor), end='')
+for i, v in enumerate(lista):
+    if v == menor:
+        print('{}...'.format(i), end='')
