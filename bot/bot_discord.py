bb104a1df94e61d556b2cc93e46959c1067b64e5
Rian Nunes <ryannunes70@gmail.com>
2023-02-05 12:28:46 -0400

    mod

---
+++ bot/bot_discord.py
@@ -0,0 +1,12 @@
+
+import lightbulb
+
+bot = lightbulb.BotApp(token=open('token_discord.txt', 'r').read(),default_enabled_guilds=(int(open('id_servidor.txt', 'r').read())))
+
+@bot.command
+@lightbulb.command('msg_r1','Saudações mortais')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def hello(ctx):
+    await ctx.respond('*Opa, 100%?*')
+
+bot.run()
